18f282c767ded4011b095c6174929db17732c5cb
Outaos <outao_eu@yahoo.com>
2022-09-14 17:50:29 +0100

    storyman cv

---
+++ csv/upload-data-here.md
@@ -0,0 +1,3 @@
+Option: upload your Google Sheets data in separate CSV files here, as described in https://handsondataviz.org/leaflet-storymaps-with-google-sheets/
+
+
---
+++ google-doc-url.js
@@ -0,0 +1,8 @@
+// paste in your published Google Sheets URL from the browser address bar
+
+ var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+// var googleDocURL = 'https://docs.google.com/spreadsheets/d/1KVjoBCPC793cUOqkchh12vEZCcd1y-0TBQuuqCkAX_o/edit?usp=sharing';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+ var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+// var googleApiKey = 'AIzaSyCt4xRNSGqekG0mABV6QRLLmBfndr3sRkM';
